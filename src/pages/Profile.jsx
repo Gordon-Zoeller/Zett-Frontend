d38b5644a437838571f05390dead55b80f57ceb8
anonymous <email>
2023-12-20 11:31:16 +0100

    create: Profile

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { Context } from "../context/Context";
+
+export default function Profile() {
+    const {user} = useContext(Context);
+    return (
+        <>
+            <h2>{user.firstName}</h2>
+        </>
+    );
+};
