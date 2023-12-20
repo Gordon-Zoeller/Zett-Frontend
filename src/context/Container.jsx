317401e2b6489c95916189f88ad82576a21e6d1b
anonymous <email>
2023-12-20 11:24:32 +0100

    create: Container; useState for user; useEffect to verify

---
+++ src/context/Container.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { Context } from "./Context";
+
+export default function Container({children}) {
+    const [user, setUser] = useState({});
+    useEffect(() => {
+        const token = sessionStorage.getItem("token");
+        if(token) {
+            async function verify() {
+                try {
+                    const response = await fetch(`${import.meta.env.VITE_VERIFY_TOKEN}`, {method: "GET", headers: {token: token}});
+                    if(response.ok) {
+                        const data = await response.json();
+                        if(data.success) {
+                            setUser(data.data);
+                        };
+                    }
+                } catch (error) {
+                    //
+                }
+            };
+            verify();
+        };
+    }, []);
+    return (
+        <>
+            <Context.Provider value={{
+                user, setUser
+            }}>
+                {children}
+            </Context.Provider>
+        </>
+    );
+};
