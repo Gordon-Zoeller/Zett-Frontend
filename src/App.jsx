e6bdbe72f62ad44c48215dfbc97edfe1053675fe
anonymous <email>
2023-12-21 09:50:57 +0100

    update: NavLink and Route for Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { NavLink, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home';
+import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Register from './pages/Register';
 
@@ -15,6 +16,7 @@ function App() {
           <ul>
             <li><NavLink to="/">Home</NavLink></li>
             <li><NavLink to="/register">Register</NavLink></li>
+            <li><NavLink to="/login">Login</NavLink></li>
             <li><NavLink to="/profile">Profile</NavLink></li>
           </ul>
         </nav>
@@ -23,6 +25,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
           <Route path="/profile" element={<Profile/>}/>
         </Routes>
       </main>
