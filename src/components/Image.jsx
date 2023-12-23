c4892369b23680c25a14f7c76c040106b33c10f8
anonymous <email>
2023-12-23 10:41:08 +0100

    create: Image

---
+++ src/components/Image.jsx
@@ -0,0 +1,10 @@
+export default function Image() {
+    return (
+        <>
+            <div>
+                <label htmlFor="image">Image</label>
+                <input type="text" name="image" id="image" />
+            </div>
+        </>
+    );
+};
