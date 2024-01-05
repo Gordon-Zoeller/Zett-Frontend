67a3ea3bbdf088f1c3c62fabfcb5297d33b2f24e
anonymous <email>
2024-01-05 11:15:08 +0100

    create: LoginInformation

---
+++ src/components/LoginInformation.jsx
@@ -0,0 +1,11 @@
+import Email from "./ui/Email";
+import Password from "./ui/Password";
+
+export default function LoginInformation() {
+    return (
+        <>
+            <Email/>
+            <Password/>
+        </>
+    );
+};
