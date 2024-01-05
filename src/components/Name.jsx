a288147bf0eaef8decf36b2fc2a70b3efe7be005
anonymous <email>
2024-01-05 11:14:33 +0100

    create: Name

---
+++ src/components/Name.jsx
@@ -0,0 +1,11 @@
+import FirstName from "./ui/FirstName";
+import LastName from "./ui/LastName";
+
+export default function name() {
+    return (
+        <>
+            <FirstName/>
+            <LastName/>
+        </>
+    );
+};
