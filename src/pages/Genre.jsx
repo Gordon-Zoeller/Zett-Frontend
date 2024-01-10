57871c220de40eb99bb783653d244865b7d912d4
anonymous <email>
2024-01-10 15:15:58 +0100

    update: changed div to BooksContainer; imported BookCard; create: booksByGenre function

--- src/pages/Genre.jsx
+++ src/pages/Genre.jsx
@@ -1,11 +1,39 @@
+import { useContext, useEffect } from "react";
+import { Context } from "../context/Context";
 import { useLocation } from "react-router-dom";
+import BookCard from "../components/BookCard";
+import BooksContainer from "../components/BooksContainer";
 
 export default function Genre() {
     const {state} = useLocation();
+    const {books, setBooks} = useContext(Context);
+    useEffect(() => {
+        async function booksByGenre() {
+            try {
+                const response = await fetch(`${import.meta.env.VITE_BOOKS_BY_GENRE}${state}`);
+                if(response.ok) {
+                    const data = await response.json();
+                    if(data.success) setBooks(data.data);
+                };
+            } catch (error) {
+                //
+            };
+        };
+        booksByGenre();
+    }, [state]);
     return (
         <>
-            <h4>Genre</h4>
-            <h5>{state}</h5>
+            <BooksContainer> //component reusable in SearchResult
+                {
+                    books.map(book => {
+                        return (
+                            <>
+                                <BookCard book={book}/>
+                            </>
+                        );
+                    })
+                }
+            </BooksContainer>
         </>
     );
 };
