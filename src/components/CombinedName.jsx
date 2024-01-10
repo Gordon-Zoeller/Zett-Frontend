a6bf8d51c1d058079ac2197b2d8fb05f73792d2d
anonymous <email>
2024-01-10 16:01:10 +0100

    create: file

---
+++ src/components/CombinedName.jsx
@@ -0,0 +1,8 @@
+export default function CombinedName() {
+    return (
+        <>
+            <label htmlFor="combinedName">Name</label>
+            <input type="text" name="combinedName" id="combinedName" />
+        </>
+    );
+};
