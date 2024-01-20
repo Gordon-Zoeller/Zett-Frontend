182beab109158ed2544c7068337e43828b26219a
anonymous <email>
2024-01-20 11:58:12 +0100

    update: moved booksByGenre function to services

--- src/pages/Genre.jsx
+++ src/pages/Genre.jsx
@@ -3,23 +3,13 @@ import { Context } from "../context/Context";
 import { useLocation } from "react-router-dom";
 import BookCard from "../components/BookCard";
 import Bookshelf from "../components/Bookshelf";
+import { byGenre } from "../services/api/books/ByGenre";
 
 export default function Genre() {
     const {state} = useLocation();
     const {books, setBooks} = useContext(Context);
     useEffect(() => {
-        async function booksByGenre() {
-            try {
-                const response = await fetch(`${import.meta.env.VITE_BOOKS_BY_GENRE}${state}`);
-                if(response.ok) {
-                    const data = await response.json();
-                    if(data.success) setBooks(data.data);
-                };
-            } catch (error) {
-                //
-            };
-        };
-        booksByGenre();
+        byGenre(state, setBooks);
     }, [state]);
     return (
         <>
