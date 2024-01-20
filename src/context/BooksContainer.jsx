9ef0a7a1463b835ca67852f6a6697bde67416147
anonymous <email>
2024-01-20 11:12:34 +0100

    create: BooksContainer

---
+++ src/context/BooksContainer.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Context } from "./Context";
+
+export default function BooksContainer({children}) {
+    const [books, setBooks] = useState([]);
+    return (
+        <>
+            <Context.Provider value={{
+                books, setBooks
+            }}>
+                {children}
+            </Context.Provider>
+        </>
+    );
+};
