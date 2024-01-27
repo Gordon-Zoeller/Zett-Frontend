e7bdf76606d8a220dc600b93fd67b76eecb27b61
anonymous <email>
2024-01-27 11:50:36 +0100

    update: changed Books to Genres

---
+++ src/pages/Genres.jsx
@@ -0,0 +1,42 @@
+import { useContext, useEffect } from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import { GenreContext } from "../context/Context";
+
+export default function Genres() {
+    const {genres, setGenres} = useContext(GenreContext);
+    useEffect(() => {
+        async function getGenres() {
+            try {
+                const response = await fetch(`${import.meta.env.VITE_GENRES}`);
+                if(response.ok) {
+                    const data = await response.json();
+                    if(data.success) {
+                        setGenres(data.data);
+                    };
+                };
+            } catch (error) {
+                //
+            }
+        };
+        getGenres();
+    }, []);
+    return (
+        <>
+            <nav>
+                {
+                    genres.map(genre => {
+                        const snakeCase = genre.genre.replaceAll(" ", "_");
+                        return (
+                            <>
+                                <h3 key={genre._id}><NavLink to={`/books/genre/${snakeCase}`} state={snakeCase}>{genre.genre}</NavLink></h3>
+                            </>
+                        );
+                    })
+                }
+            </nav>
+            
+                <Outlet/>
+            
+        </>
+    );
+};
