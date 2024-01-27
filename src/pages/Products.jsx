cd7cd1066ea63ea7a4edf7c643b256f409fa706b
anonymous <email>
2024-01-27 11:51:15 +0100

    update: changed Genres to Products

---
+++ src/pages/Products.jsx
@@ -0,0 +1,29 @@
+import { useContext, useEffect } from "react";
+import { ProductContext } from "../context/Context";
+import { useLocation } from "react-router-dom";
+import BookCard from "../components/BookCard";
+import Productshelf from "../components/ProductShelf";
+import { byGenre } from "../services/api/books/ByGenre";
+
+export default function Products() {
+    const {state} = useLocation();
+    const {products, setProducts} = useContext(ProductContext);
+    useEffect(() => {
+        byGenre(state, setProducts);
+    }, [state]);
+    return (
+        <>
+            <Productshelf>
+                {
+                    products.map(product => {
+                        return (
+                            <>
+                                <BookCard product={product}/>
+                            </>
+                        );
+                    })
+                }
+            </Productshelf>
+        </>
+    );
+};
