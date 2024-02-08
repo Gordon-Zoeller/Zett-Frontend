61889d9568952f3eb2961104bb48ea7e53aec432
anonymous <email>
2024-02-08 09:31:24 +0100

    update: changed JSON to FormData

--- src/services/api/user/SignIn.jsx
+++ src/services/api/user/SignIn.jsx
@@ -1,11 +1,8 @@
 export const signin = async (e, setUser, navigate) => {
     e.preventDefault();
-    const user = {
-        email: e.target.email.value,
-        password: e.target.password.value,
-    };
     try {
-        const response = await fetch(`${import.meta.env.VITE_BASE_URL_USER}${import.meta.env.VITE_SIGNIN}`, {method: "POST",headers: {"Content-Type": "application/json"}, body: JSON.stringify(user)});
+        const user = new FormData(e.target);
+        const response = await fetch(`${import.meta.env.VITE_BASE_URL_USER}${import.meta.env.VITE_SIGNIN}`, {method: "POST", body: user});
         if(response.ok) {
             const token = response.headers.get("token");
             if(token) {
