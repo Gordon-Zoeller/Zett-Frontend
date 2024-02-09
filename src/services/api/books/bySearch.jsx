d55818f703226fe583a6fe5a426949404084ce0c
anonymous <email>
2024-02-09 11:14:19 +0100

    update: modified env for search

--- src/services/api/books/bySearch.jsx
+++ src/services/api/books/bySearch.jsx
@@ -1,6 +1,6 @@
 export const booksBySearch = async (path, setProducts) => {
     try {
-        const response = await fetch(`${import.meta.env.VITE_BASE_URL_BOOKS}${import.meta.env.VITE_BOOKS_BY_SEARCH}${path}`);
+        const response = await fetch(`${import.meta.env.VITE_BASE_URL_SEARCH}${path}`);
         if(response.ok) {
             const data = await response.json();
             if(data.success) setProducts(data.data);
