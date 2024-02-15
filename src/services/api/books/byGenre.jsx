33cf3d63cf927f85c423f58bc398f4c27bf91be1
anonymous <email>
2024-02-15 14:32:18 +0100

    update: url

--- src/services/api/books/byGenre.jsx
+++ src/services/api/books/byGenre.jsx
@@ -1,6 +1,6 @@
-export const booksByGenre = async (state, setProducts) => {
+export const booksByGenre = async (snake, setProducts) => {
     try {
-        const response = await fetch(`${import.meta.env.VITE_BASE_URL_BOOKS}${import.meta.env.VITE_BOOKS_BY_GENRE}${state}`);
+        const response = await fetch(`${import.meta.env.VITE_BASE_URL_BOOKS}${import.meta.env.VITE_BY_GENRE}${snake}`);
         if(response.ok) {
             const data = await response.json();
             if(data.success) setProducts(data.data);
