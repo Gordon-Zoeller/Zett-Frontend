35a9dd66190a06616bcf41f1e644eaaa2729b606
anonymous <email>
2024-02-15 14:33:53 +0100

    update: fetch data depending on props

--- src/services/api/genres/getGenres.jsx
+++ src/services/api/genres/getGenres.jsx
@@ -1,11 +1,16 @@
-export const getGenres = async (setGenres) => {
+export const getGenres = async (setGenres, props) => {
     try {
-        const response = await fetch(`${import.meta.env.VITE_BASE_URL_GENRES}${import.meta.env.VITE_GENRES_BOOKS}`);
+        let response;
+        switch(props.category) {
+            case "books": response = await fetch(`${import.meta.env.VITE_BASE_URL_GENRES}${import.meta.env.VITE_GENRES_BOOKS}`);
+                break;
+            case "movies": response = await fetch(`${import.meta.env.VITE_BASE_URL_GENRES}${import.meta.env.VITE_GENRES_MOVIES}`);
+                break;
+            default: {};
+        };
         if(response.ok) {
             const data = await response.json();
-            if(data.success) {
-                setGenres(data.data);
-            };
+            if(data.success) setGenres(data.data);
         };
     } catch (error) {
         //
