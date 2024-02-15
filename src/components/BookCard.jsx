a48a203a122182b147147d919ecfd0424d646a33
anonymous <email>
2024-02-15 14:28:41 +0100

    update: added question marks to Cover and Price

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -7,18 +7,18 @@ export default function BookCard({product}) {
     return (
         <>
             <div>
-                <Cover cover={product.edition.hardcover.image.thumbnail}/>
+                <Cover cover={product?.edition?.hardcover?.image?.thumbnail}/>
                 <Title title={product.title}/>
                 <Author author={product.author}/>
                 <div>
                     <p>Hardcover</p>
                 </div>
-                <Price price={product.edition.hardcover.price}/>
+                <Price price={product?.edition?.hardcover?.price}/>
                 <hr />
                 <div>
                     <p>Paperback</p>
                 </div>
-                <Price price={product.edition.paperback.price}/>
+                <Price price={product?.edition?.paperback?.price}/>
             </div>
         </>
     );
