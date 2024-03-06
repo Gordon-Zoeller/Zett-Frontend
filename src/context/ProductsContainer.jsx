c2ff5abed021a70ea881e3d06cb19d7973a46af0
anonymous <email>
2024-03-06 15:03:39 +0100

    create: ref to keep selected category on reload

--- src/context/ProductsContainer.jsx
+++ src/context/ProductsContainer.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ProductContext } from "./Context";
 
 export default function ProductsContainer({children}) {
     const [products, setProducts] = useState([]);
+    const formRef = useRef();
+    useEffect(() => {
+        if(window.location.href.includes("/books/search?q=")) {
+            formRef.current.value = "books";
+        } else if(window.location.href.includes("/movies/search?")) {
+            formRef.current.value = "movies"
+        }
+    }, []);
     return (
         <>
             <ProductContext.Provider value={{
                 products, setProducts,
+                formRef
             }}>
                 {children}
             </ProductContext.Provider>
