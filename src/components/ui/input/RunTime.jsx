81d3ea913acd11e15536e36315e057d8582612de
anonymous <email>
2024-03-07 11:29:40 +0100

    create: file

---
+++ src/components/ui/input/RunTime.jsx
@@ -0,0 +1,10 @@
+export default function RunTime() {
+    return (
+        <>
+            <div>
+                <label htmlFor="runTime">Run time</label>
+                <input type="number" name="runTime" id="runTime" />
+            </div>
+        </>
+    );
+};
