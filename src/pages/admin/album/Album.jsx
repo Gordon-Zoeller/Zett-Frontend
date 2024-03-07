4807ad4b9eb0a35d29136e6569c31b94ccf1957e
anonymous <email>
2024-03-07 11:52:43 +0100

    create: file

---
+++ src/pages/admin/album/Album.jsx
@@ -0,0 +1,64 @@
+import { useContext, useState } from "react";
+import { FaMinus, FaPlus } from "react-icons/fa";
+import Edition from "../../../components/Edition";
+import ProductInformation from "../../../components/ProductInformation";
+import RunTime from "../../../components/ui/input/RunTime";
+import { UserContext } from "../../../context/Context";
+import { plus } from "../../../helpers/Plus";
+import { minus } from "../../../helpers/Minus";
+import IMN from "../../../components/ui/input/IMN";
+import { uploadAlbum } from "../../../services/api/albums/uploadAlbum";
+import UploadBtn from "../../../components/ui/btn/UploadBtn";
+
+export default function Album() {
+    const [tracks, setTracks] = useState([]);
+    const {user} = useContext(UserContext);
+    return (
+        <>
+            {
+                user?.role === "admin" &&
+                <form onSubmit={uploadAlbum}>
+                    <div>
+                        <label htmlFor="artist">Artist</label>
+                        <input type="text" name="artist" id="artist" />
+                    </div>
+                    <ProductInformation>
+                        <RunTime/>
+                        <IMN/>
+                    </ProductInformation>
+                    <div>
+                        <label htmlFor="track">Track</label>
+                        <input type="text" name="track" id="track" />
+                        <button type="button" onClick={() => plus(tracks, setTracks)}>
+                            <FaPlus className="icon"/>
+                        </button>
+                    </div>
+                    {
+                        tracks.map((id, index) => {
+                            return (
+                                <>
+                                    <div>
+                                        <label htmlFor={`track${index}`}>Track</label>
+                                        <input type="text" name={`track${index}`} id={`track${index}`} />
+                                        <button type="button" onClick={() => minus(id, tracks, setTracks)}>
+                                            <FaMinus className="icons"/>
+                                        </button>
+                                    </div>
+                                </>
+                            );
+                        })
+                    }
+                    <div>
+                        <h6>CD</h6>
+                    </div>
+                    <Edition/>
+                    <div>
+                        <h6>Vinyl</h6>
+                    </div>
+                    <Edition/>
+                    <UploadBtn/>
+                </form>
+            }
+        </>
+    );
+};
