6c9c27bbca1db86626439742ec51116e2fd46137
anonymous <email>
2024-03-07 15:19:07 +0100

    update: corrected body of fetch

--- src/services/api/albums/uploadAlbum.jsx
+++ src/services/api/albums/uploadAlbum.jsx
@@ -4,7 +4,7 @@ export const uploadAlbum = async e => {
         const album = new FormData(e.target);
         const token = sessionStorage.getItem("token");
         if(token) {
-            const response = await fetch(`${import.meta.env.VITE_BASE_URL_ALBUMS}${import.meta.env.VITE_UPLOAD}`, {method: "POST", headers: {token: token, body: album}});
+            const response = await fetch(`${import.meta.env.VITE_BASE_URL_ALBUMS}${import.meta.env.VITE_UPLOAD}`, {method: "POST", headers: {token: token}, body: album});
             if(response.ok) {
                 const data = await response.json();
                 if(data.success) console.log(data.message);
