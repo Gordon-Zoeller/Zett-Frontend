8292bd248c5ee6efffd6a5f5a9895cadbce117e1
anonymous <email>
2024-03-07 11:53:27 +0100

    create: file

---
+++ src/components/ui/input/IMN.jsx
@@ -0,0 +1,10 @@
+export default function IMN() {
+    return (
+        <>
+            <div>
+                <label htmlFor="imn">IMN</label>
+                <input type="number" name="imn" id="imn" />
+            </div>
+        </>
+    );
+};
