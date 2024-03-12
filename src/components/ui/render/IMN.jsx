e689f54bb40b5eb651ac9b87e6e1be156a02e2e8
anonymous <email>
2024-03-12 14:25:07 +0100

    create: file

---
+++ src/components/ui/render/IMN.jsx
@@ -0,0 +1,7 @@
+export default function IMN({imn}) {
+    return (
+        <>
+            <p>{imn}</p>
+        </>
+    );
+};
---
+++ src/components/ui/render/RunTime.jsx
@@ -0,0 +1,7 @@
+export default function RunTime({runTime}) {
+    return (
+        <>
+            <p>{runTime}</p>
+        </>
+    );
+};
